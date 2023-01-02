f213b15620e92a7147eb090faa5c6a1ff4e7eec7
mirats-sde <101569551+mirats-sde@users.noreply.github.com>
2023-01-02 10:00:37 +0530

    ssl updated

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const BLAZE_BASE_URL = "http://blaze.miratsoneservices.com/api/v1";
 export const CLIENT_BASE_URL =
-  "http://client.miratsoneservices.com/api/v1/client";
-export const SALES_BASE_URL = "http://sales.miratsoneservices.com/api/v1";
+  "https://client.miratsoneservices.com/api/v1/client";
+export const SALES_BASE_URL = "https://sales.miratsoneservices.com/api/v1";
 export const PEOPLES_BASE_URL =
-  "http://peoples.miratsoneservices.com/api/v1/peoples";
-export const FILE_UPLOAD_URL = "http://storage.miratsoneservices.com";
+  "https://peoples.miratsoneservices.com/api/v1/peoples";
+export const FILE_UPLOAD_URL = "https://storage.miratsoneservices.com";
