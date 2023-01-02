8d5cda456219949b5647f9a4c473e75f4dcb6f0d
mirats-sde <101569551+mirats-sde@users.noreply.github.com>
2023-01-02 10:13:22 +0530

    v1 chages

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const BLAZE_BASE_URL = "http://blaze.miratsoneservices.com/api/v1";
 export const CLIENT_BASE_URL =
-  "https://client.miratsoneservices.com/api/v1/client";
+  "http://client.miratsoneservices.com/api/v1/client";
 export const SALES_BASE_URL = "https://sales.miratsoneservices.com/api/v1";
 export const PEOPLES_BASE_URL =
-  "https://peoples.miratsoneservices.com/api/v1/peoples";
+  "http://peoples.miratsoneservices.com/api/v1/peoples";
 export const FILE_UPLOAD_URL = "https://storage.miratsoneservices.com";
